4d20230976251e8c7819537afbf51bd263fe7fb0
Ahmed Amer <ahmed_amir30@icloud.comn>
2025-11-06 22:00:46 +0000

    fixed sim prime hopefully

--- dist/raydiumCtx.js
+++
@@ -1,9 +0,0 @@
-import { Connection, Keypair } from "@solana/web3.js";
-import { ApiV3 } from "@raydium-io/raydium-sdk-v2";
-export async function initRaydiumCtx(rpcUrl) {
-    const connection = new Connection(rpcUrl, "confirmed");
-    const api = new ApiV3(); // uses Raydium API endpoints internally
-    const wallet = { publicKey: Keypair.generate().publicKey }; // dummy for readonly
-    return { api, connection, wallet };
-}
-//# sourceMappingURL=raydiumCtx.js.map
